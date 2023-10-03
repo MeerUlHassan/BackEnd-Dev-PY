55faff854d9c1663aa0a4c59a79b46723910df6b
MeerUlHassan <github4421@gmail.com>
2023-10-03 03:19:19 -0700

    Basic Ope

---
+++ basic_ope.py
@@ -0,0 +1,25 @@
+first_name = "Meer"
+last_name = "UL Hassan"
+full_name = first_name +" "+ last_name
+print(full_name)
+
+
+num1=int(input("Enter first number ?"))
+num2=int(input("Enter 2nd number ?"))
+total=num1 + num2
+print("Total is ",total)
+
+input two values in one line 
+name, age =input("Enter your name and age").split()
+print(name)
+print(age)
+
+Print name in reverse order.
+name= input("Enter your name ?")
+print(name[-1::-1])
+
+
+
+
+
+
