aa28df9515be3781b699b346fe66cfc0688d7b23
MeerUlHassan <github4421@gmail.com>
2023-10-08 10:20:15 -0700

    some list methods

---
+++ List_method.py
@@ -0,0 +1,18 @@
+myList = [11,2,31,56,12,3,14,2]
+
+# myList.append(10)  #it will insert the item to the end of the list
+# myList.sort()  #it will sort the list (assending order)
+# myList.sort(reverse=True)  # it will sort the list backwards(descending order)
+# myList.reverse() # reverse the list (last item to the first)
+# print(myList.index(31))  it tells us the index of the item presnet in the list
+# print(myList.count(3))  it counts how many times the item presnet in the list
+# newList = myList  if we make a change in newList the change will also commense in mylist
+# newList[0] = 10
+# newList[1] = 5
+# print(newList)
+# myList.insert(0, 1)  #you can insert an item at any given index using insert function
+# nlist = myList
+# nlist.insert(0, 1) #you can insert an item at any given index
+# print(nlist) 
+# myList.remove(2) it remove the item from the list
+print(myList) 
