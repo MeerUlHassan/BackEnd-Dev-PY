e86e4c9c788cca4901703dfdb08155e02fbe7cca
MeerUlHassan <github4421@gmail.com>
2023-10-09 04:37:50 -0700

    added a method

--- List_method.py
+++ List_method.py
@@ -1,6 +1,6 @@
 myList = [11,2,31,56,12,3,14,2]
 
-# myList.append(10)  #it will insert the item to the end of the list
+myList.append(10)  #it will insert the item to the end of the list
 # myList.sort()  #it will sort the list (assending order)
 # myList.sort(reverse=True)  # it will sort the list backwards(descending order)
 # myList.reverse() # reverse the list (last item to the first)
@@ -15,4 +15,7 @@ myList = [11,2,31,56,12,3,14,2]
 # nlist.insert(0, 1) #you can insert an item at any given index
 # print(nlist) 
 # myList.remove(2) it remove the item from the list
+# myList.pop(1) it will pop item from the list at given index
+
+
 print(myList) 
