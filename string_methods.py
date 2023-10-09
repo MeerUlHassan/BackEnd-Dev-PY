40e896c6bb0d6010065d028fb4652b17bfe6ab1e
MeerUlHassan <github4421@gmail.com>
2023-10-09 04:54:17 -0700

    string rename

---
+++ string_methods.py
@@ -0,0 +1,50 @@
+# first_name = "Meer"
+# last_name = "UL Hassan"
+# full_name = first_name +" "+ last_name
+# print(full_name)
+
+# # num1=int(input("Enter first number ?"))
+# num2=int(input("Enter 2nd number ?"))
+# total=num1 + num2
+# print("Total is ",total)
+
+# input two values in one line 
+# name, age =input("Enter your name age", "Enter your age")
+# name, age =input("Enter your name and age").split()
+# print(name)
+# print(age)
+
+# EXERCISE
+# num1,num2,num3 = eval(input("Enter three numbers seperated by commas :"))
+# average=(num1+num2+num3)/3
+# print("Average is ",average)
+
+# isnumeric method
+
+# string = "@12345"
+# print(string.isnumeric())
+
+# name = " "
+# print(name.join("MEERHASSAN"))
+
+# newList = ["I", 'AM', 'LEARING', 'PYTHON']
+# print("-".join(newList))
+
+
+# print("this is another example".split(""))
+
+# name = input("Enter your name :")
+# lname = input("Enter last name :")
+# # print(f"Wellcome {name}")
+# print("Hello {name} {lname}".format(name=name, lname=lname))
+
+# price = 100
+# taxPrice = price * 1.50
+# print(price, taxPrice)
+# print(" Base Price ${:.2f}, Tax Price ${:.2f}".format(price, taxPrice))
+
+def to_celsius(x):
+    return (x-32) * 5/9
+
+for x in range(0, 101, 10):
+    print("{:>3} F | {:>6.2f} C".format(x, to_celsius(x)))
