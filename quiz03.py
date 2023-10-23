ed31a28d6374329d98accc619216ddddf90f7103
MeerUlHassan <github4421@gmail.com>
2023-10-23 02:55:10 -0700

    quiz3 Done

---
+++ quiz03.py
@@ -0,0 +1,6 @@
+# DICTIONARY QUIZ
+
+cool_beasts = {'octopous': 'tentacles', "dolphins": "fines" , "rhinos": "horns"}
+
+for key, value in cool_beasts.items():
+    print("{} have {}".format(key, value))
